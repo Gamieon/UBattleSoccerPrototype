79359d8c2510702be05f20034263a48a008ff7fc
Christopher Haag <c.haag@gamieon.com>
2014-10-18 10:23:01 -0400

    - Refactored directory structure to reflect that of the ShooterGame demo project

--- Source/MagicBattleSoccer/MagicBattleSoccer.Build.cs
+++ Source/MagicBattleSoccer/MagicBattleSoccer.Build.cs
@@ -6,12 +6,29 @@ public class MagicBattleSoccer : ModuleRules
 {
 	public MagicBattleSoccer(TargetInfo Target)
 	{
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+        PrivateIncludePaths.AddRange(
+            new string[] { 
+                "MagicBattleSoccer/Classes/Bots"
+                ,"MagicBattleSoccer/Classes/Online"
+				,"MagicBattleSoccer/Classes/Player"
+                ,"MagicBattleSoccer/Classes/Soccer"
+                ,"MagicBattleSoccer/Classes/Weapons"
+            }
+        );
+
+        PublicDependencyModuleNames.AddRange(new string[] { 
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "InputCore", 
+            "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { 
+            "Slate", 
+            "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
